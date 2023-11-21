74539a27efcd877f3856d63c5758c38a507c5335
anamariaow <new_annemarie@yahoo.com>
2023-11-21 16:53:11 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
         Auto auto1 = new Auto(1800, "TI785WY", "Honda", "Civic");
 
         System.out.println("Auto 1: " + auto1);
+
+        System.out.println("Cilindrata: " + auto1.getCilindrata());
+        System.out.println("Targa: " + auto1.getTarga());
+        System.out.println("Marca: " + auto1.getMarca());
+        System.out.println("Modello: " + auto1.getModello());
+
+        auto1.setCilindrata(2000);
+        System.out.println("Nuova cilindrata: " + auto1.getTarga());
     }
 }
